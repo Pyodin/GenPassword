1aaeeb62852b090b64da3b1bf3eefdc8b55e8f4d
Paul Bourhis <paul_bourhis@yahoo.com>
2024-06-05 11:17:03 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# GenPassword
+This is a simple app that generates a password for you. You can choose the length of the password and the characters that you want to include in the password. The app will generate a password for you based on your choices.
+
+
---
+++ core/views.py
@@ -0,0 +1,60 @@
+from string import ascii_lowercase, ascii_uppercase, digits
+import random
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    if request.method == "POST":
+        form = request.POST
+        if not any(
+            key in form
+            for key in [
+                "numbersChecked",
+                "lowerChecked",
+                "upperChecked",
+                "specialChecked",
+            ]
+        ):
+            return render(
+                request,
+                "core/index.html",
+                {
+                    "message": "Cochez au moins une option pour générer un mot de passe.",
+                    "form": form,
+                },
+            )
+        
+        length = int(form["length"])
+
+        allowedChars = ""
+        if "numbersChecked" in form:
+            allowedChars += digits
+        if "lowerChecked" in form:
+            allowedChars += ascii_lowercase
+        if "upperChecked" in form:
+            allowedChars += ascii_uppercase
+        if "specialChecked" in form:
+            allowedChars += "!@#$%^&*()_+{}|:<>?"
+
+        res = {
+            f"pass_{i}": "".join(random.choice(allowedChars) for _ in range(length))
+            for i in range(21)
+        }
+        return render(request, "core/index.html", {"password": res, "form": form})
+    else:
+
+        return render(
+            request,
+            "core/index.html",
+            {
+                "form": {
+                    "length": 12,
+                    "numbersChecked": "on",
+                    "lowerChecked": "on",
+                    "upperChecked": "on",
+                }
+            },
+        )
